8708b4f0ea7367d96806ac70ee5d80b920831c26
fan <291950304@qq.com>
2024-08-22 10:42:39 +0800

    chore: webpack 处理静态资源

--- code/webpack/01.basic/webpack.config.js
+++ code/webpack/01.basic/webpack.config.js
@@ -1,4 +1,5 @@
 const { resolve } = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
@@ -10,40 +11,65 @@ module.exports = () => ({
   },
   output: {
     filename: '[name].js',
-    path: resolve(__dirname, './dist')
+    path: resolve(__dirname, './dist'),
+    // assetModuleFilename: 'images/[name].[ext]',
+    clean: true
   },
   module: {
-    rules: [{
-      test: /\.css$/i,
-      use: [
-        // 根据运行环境判断使用那个 loader
-        (process.env.NODE_ENV === 'development' ?
-          'style-loader' :
-          MiniCssExtractPlugin.loader),
-        'css-loader',
-        {
-          loader: "postcss-loader",
-          options: {
-            postcssOptions: {
-              // 添加 autoprefixer 插件
-              // plugins: [require("autoprefixer")],
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          // 根据运行环境判断使用那个 loader
+          (process.env.NODE_ENV === 'development' ?
+            'style-loader' :
+            MiniCssExtractPlugin.loader),
+          'css-loader',
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                // 添加 autoprefixer 插件
+                // plugins: [require("autoprefixer")],
+              },
             },
-          },
+          }
+        ]
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          // 根据运行环境判断使用那个 loader
+          (process.env.NODE_ENV === 'development' ?
+            'style-loader' :
+            MiniCssExtractPlugin.loader),
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      // {
+      //   test: /\.(png|jpe?g|gif)$/i,
+      //   use: [
+      //     {
+      //       loader: 'file-loader',
+      //       options: {
+      //         name: '[name].[ext]',
+      //         outputPath: 'images'
+      //       }
+      //     }
+      //   ]
+      // },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[hash:8][ext]'
         }
-      ]
-    }, {
-      test: /\.less$/i,
-      use: [
-        // 根据运行环境判断使用那个 loader
-        (process.env.NODE_ENV === 'development' ?
-          'style-loader' :
-          MiniCssExtractPlugin.loader),
-        'css-loader',
-        'less-loader'
-      ]
-    }]
+      }
+    ]
   },
   plugins: [
+    // new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin()
   ]
